a465bb1efa452430d793369ebf32d369cab88c76
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-30 17:51:46 +0300

    [feat] Добавил иконку поиска

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -1,16 +1,19 @@
 <script context="module" lang="ts">
-	export type Icons = "done";
+	export type Icons = "done" | "search";
 </script>
 
 <script lang="ts">
 	import DoneIcon from "$lib/icons/DoneIcon.svelte";
+	import SearchIcon from "$lib/icons/SearchIcon.svelte";
 
 	export let icon: Icons;
 </script>
 
-<div>
+<div class="icon">
 	{#if icon === "done"}
 		<DoneIcon />
+	{:else if icon === "search"}
+		<SearchIcon />
 	{:else}
 		UNREACHABLE
 	{/if}
