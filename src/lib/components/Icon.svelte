808562649e2bce8e4ff6401e326847d9cc7a92e4
Artyom Abramyan <artemabramyan7@icloud.com>
2023-04-30 15:50:20 +0300

    [feat] Добавил компонент `Icon`
    Динамически выставляет иконку через props

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,17 @@
+<script context="module" lang="ts">
+	export type Icons = "done";
+</script>
+
+<script lang="ts">
+	import DoneIcon from "$lib/icons/DoneIcon.svelte";
+
+	export let icon: Icons;
+</script>
+
+<div>
+	{#if icon === "done"}
+		<DoneIcon />
+	{:else}
+		UNREACHABLE
+	{/if}
+</div>
