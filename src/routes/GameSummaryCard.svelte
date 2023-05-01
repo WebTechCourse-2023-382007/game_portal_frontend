998bafc866dfaac4e6f7d3656a6709ea1f81c097
Artyom Abramyan <artemabramyan7@icloud.com>
2023-05-01 10:12:06 +0300

    Написал компонент для отображения `GameSummary`

---
+++ src/routes/GameSummaryCard.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+	import type { GameSummary } from "$lib/types";
+	export let summary: GameSummary;
+</script>
+
+<a class="link" href={summary.id}>
+	<div class="card">
+		<div class="image_wrapper">
+			<img
+				class="image"
+				alt={`${summary.name} logo`}
+				src={summary.logoUrl}
+			/>
+		</div>
+		<div class="text">
+			<div class="text__heading">
+				<p class="md-on-surface-text md-title-large heading__name">
+					{summary.name}
+				</p>
+				<p class="md-on-surface-variant-text md-label-large 	heading__trivia">
+					{summary.releaseYear} • {summary.publisher}
+				</p>
+			</div>
+			<ul class="text__tag_list">
+				{#each summary.tags as tag}
+					<li class="md-on-surface-text md-label-large tag_list__tag">
+						{tag}
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
+</a>
+
+<style>
+	.link {
+		display: contents;
+	}
+
+	.card {
+		padding: 16px;
+		width: 100%;
+		height: 130px; /* very bad */
+
+		border: 1px solid var(--md-sys-color-outline-variant);
+		border-radius: 12px;
+		background: transparent;
+
+		display: flex;
+		flex-direction: row;
+		gap: 16px;
+	}
+
+	.image_wrapper {
+		width: 220px;
+		margin-left: -16px;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		flex-shrink: 0;
+	}
+
+	.image {
+		max-height: 100%;
+		object-fit: scale-down;
+		overflow: hidden;
+	}
+
+	.text {
+		display: flex;
+		flex-direction: column;
+		align-items: stretch;
+		gap: 20px;
+
+		flex-grow: 1;
+	}
+
+	.text__heading {
+		display: flex;
+		flex-direction: column;
+		gap: 0;
+		align-items: stretch;
+	}
+
+	.text__tag_list {
+		--gap: 4px;
+
+		display: flex;
+		flex-direction: row;
+		gap: var(--gap);
+
+		padding: 0;
+	}
+
+	.tag_list__tag {
+		display: inline-flex;
+		gap: var(--gap);
+	}
+
+	.tag_list__tag:not(:first-child)::before {
+		content: '•';
+	}
+</style>
